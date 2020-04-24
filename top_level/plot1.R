e26a2adb48dbe941063381d3732442b1b3ed7a88
Xiao <wang82299@gmail.com>
2020-04-24 13:48:50 -0400

    Add files via upload

--- top_level/plot1.R
+++ top_level/plot1.R
@@ -1,1 +1,11 @@
-
+#Plot1
+power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+#calling the basic plot function convert the character values into numerical values
+hist(as.numeric(as.character(subpower$Global_active_power)),col="red",
+     main="Global Active Power",xlab="Global Active Power(kilowatts)")
+# annotating graph
+title(main="Global Active Power")
+dev.copy(png, file="plot1.png",width=480, height=480)
+dev.off()
